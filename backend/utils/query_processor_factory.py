f9986644796bc31b76f5a9e86b507a4e88f438ab
bhagavan <bhagavan@auranetworks.in>
2024-12-16 14:28:28 +0530

    Mapping in a better way

--- backend/utils/query_processor_factory.py
+++ backend/utils/query_processor_factory.py
@@ -5,25 +5,24 @@ from backend.processors.mahabharata_db import MahabharataDBProcessor
 from backend.processors.programming_db import ProgrammingDBProcessor
 from backend.processors.cholas_db import CholasDBProcessor
 from backend.processors.sabarimala import SabarimalaDBProcessor
+from typing import Dict
 
 from pdbwhereami import whereami
 
 class QueryProcessorFactory:
-    
+    _PROCESSOR_MAP: Dict[str, VectorDBQueryProcessor] = {
+        "pdf-vectorDB": pdfDBProcessor,
+        "ramayan-vdb": RamayanaDBProcessor,
+        "mahabharata-vdb": MahabharataDBProcessor,
+        "progrmmingVDB": ProgrammingDBProcessor,
+        "cholas-vdb": CholasDBProcessor,
+        "sabarimala-vdb": SabarimalaDBProcessor,
+    }
+
     @staticmethod
     def get_processor(vector_db_name: str) -> VectorDBQueryProcessor:
         whereami(f"vector_db_name :{vector_db_name}")
-        if vector_db_name == "pdf-vectorDB":
-            return pdfDBProcessor()
-        elif vector_db_name == "ramayan-vdb":
-            return RamayanaDBProcessor()
-        elif vector_db_name == "mahabharata-vdb":
-            return MahabharataDBProcessor()
-        elif vector_db_name == "progrmmingVDB":
-            return ProgrammingDBProcessor()
-        elif vector_db_name == "cholas-vdb":
-            return CholasDBProcessor()
-        elif vector_db_name == "sabarimala-vdb":
-            return SabarimalaDBProcessor()
-        else:
-            raise ValueError("Unknown vector DB name")
+        try:
+            return QueryProcessorFactory._PROCESSOR_MAP[vector_db_name]()
+        except KeyError:
+            raise ValueError(f"Unknown vector DB name: {vector_db_name}")
